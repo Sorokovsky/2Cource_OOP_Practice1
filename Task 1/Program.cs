0f8dccaebc1456adf2172be230a057fa43c459c7
Sorokovsky <Sorokovskys@ukr.net>
2024-09-09 20:30:56 +0300

    task 2

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,7 +1,13 @@
-﻿public static class Program
+﻿namespace Task_1;
+public static class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello");
+        double width, height;
+        Console.Write("Enter a width: "); width = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter a height: "); height = Convert.ToDouble(Console.ReadLine());
+        Rectangle rectangle = new Rectangle(width, height);
+        Console.WriteLine("Area: {0}.", rectangle.Area);
+        Console.WriteLine("Perimeter: {0}.", rectangle.Perimeter);
     }
 }
---
+++ Task 1/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_1
+{
+    public class Rectangle
+    {
+        private double _side1, _side2;
+        public Rectangle(double side1, double side2)
+        {
+            _side1 = side1;
+            _side2 = side2;
+        }
+
+        public double Area => AreaCalculator();
+
+        public double Perimeter => PerimeterCalculator();
+
+        public double AreaCalculator()
+        {
+            return _side1 * _side2;
+        }
+
+        public double PerimeterCalculator()
+        {
+            return 2 * (_side1 + _side2);
+        }
+    }
+}
