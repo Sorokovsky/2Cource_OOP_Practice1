74c6aaa22b04f0b247250dcaf546b37e000c8c98
Sorokovsky <Sorokovskys@ukr.net>
2024-09-10 21:24:47 +0300

    task 6

--- Task  6/Program.cs
+++ Task  6/Program.cs
@@ -3,6 +3,13 @@ public static class Program
 {
     public static void Main()
     {
-
+        string login, name, surname;
+        int age;
+        Console.Write("Enter a login: "); login = Console.ReadLine();
+        Console.Write("Enter a name: "); name = Console.ReadLine();
+        Console.Write("Enter a surname: "); surname = Console.ReadLine();
+        Console.Write("Enter a age: "); age = Convert.ToInt32(Console.ReadLine());
+        User user = new User(login, name, surname, age);
+        user.Show();
     }
 }
---
+++ Task  6/User.cs
@@ -0,0 +1,48 @@
+﻿namespace Task_6
+{
+    internal class User
+    {
+        private int _age;
+
+        public string Login { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; init; }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if(value < 1)
+                {
+                    _age = 1;
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        public User(string login, string name, string surname, int age)
+        {
+            Login = login;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            CreatedAt = DateTime.Now;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Login: {0}.", Login);
+            Console.WriteLine("Name: {0}.", Name);
+            Console.WriteLine("Surname: {0}.", Surname);
+            Console.WriteLine("Age: {0}.", Age);
+            Console.WriteLine("Created at: {0}.", CreatedAt);
+        }
+    }
+}
