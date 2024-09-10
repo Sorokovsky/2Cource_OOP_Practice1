f41061db5200e50ec4e85f6f4d3056f9a95611a0
Sorokovsky <Sorokovskys@ukr.net>
2024-09-10 22:53:05 +0300

    task 7

---
+++ Task 7/Converter.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_7
+{
+    internal class Converter
+    {
+        private double _usd, _eur, _uan;
+        public Converter(double usd, double eur, double uan)
+        {
+            _usd = usd;
+            _eur = eur;
+            _uan = uan;
+        }
+
+        public double FromUSD(double usd)
+        {
+            return usd * _usd;
+        }
+
+        public double FromEUR(double eur)
+        {
+            return eur * _eur;
+        }
+
+        public double ToUSD(double uan)
+        {
+            return uan / _usd;
+        }
+
+        public double ToEUR(double uan)
+        {
+            return uan / _eur;
+        }
+    }
+}
---
+++ Task 7/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_7;
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a curse of uah to usa: "); double usd = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter a curse of uah to eur: "); double eur = Convert.ToDouble(Console.ReadLine());
+        Converter converter = new Converter(usd, eur, 1);
+        Console.Write("Enter a how much have esa: "); double howUsd = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("{0}usd -> {1}uah", howUsd, converter.FromUSD(howUsd));
+        Console.Write("Enter a how much have eur: "); double howEur = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("{0}eur -> {1}uah", howEur, converter.FromUSD(howEur));
+        Console.Write("Enter a how much have uah: "); double howUah = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("{0}uah -> {1}eur", howUah, converter.ToEUR(howUah));
+        Console.WriteLine("{0}uah -> {1}esd", howUah, converter.ToUSD(howUah));
+    }
+}
