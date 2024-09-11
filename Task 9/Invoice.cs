7469df79d9e22d5a3ea586bdc59e99a11c5daab0
Sorokovsky <Sorokovskys@ukr.net>
2024-09-11 20:26:25 +0300

    task 9

---
+++ Task 9/Invoice.cs
@@ -0,0 +1,38 @@
+﻿namespace Task_9
+{
+    public class Invoice
+    {
+        private static int _accountNumber = 0;
+        private static int _pdv = 2;
+
+        private string _article = Guid.NewGuid().ToString();
+        private int _quantity;
+
+        public int Account { get; init; }
+        public string Costumer { get; init; } = string.Empty;
+        public string Provider {  get; init; } = string.Empty;
+
+        public Invoice(int account, string costumer, string provider, int quantity)
+        {
+            Account = account;
+            Costumer = costumer;
+            Provider = provider;
+            _quantity = quantity;
+        }
+
+        public int WithoutPdv()
+        {
+            return _quantity;
+        }
+
+        public int WithPdv()
+        {
+            return _quantity * _pdv;
+        }
+
+        public static Invoice Create(string costumer, string provider, int quantity)
+        {
+            return new Invoice(_accountNumber++, costumer, provider, quantity);
+        }
+    }
+}
--- Task 9/Program.cs
+++ Task 9/Program.cs
@@ -1,7 +1,20 @@
-﻿public static class Program
+﻿namespace Task_9;
+
+public static class Program
 {
     public static void Main()
     {
+        Console.WriteLine("Enter a invoice.");
+        Invoice invoice = EnterInvoice();
+        Console.WriteLine("Salary without pdv: {0}.", invoice.WithoutPdv());
+        Console.WriteLine("Salary with pdv: {0}.", invoice.WithPdv());
+    }
 
+    public static Invoice EnterInvoice()
+    {
+        Console.Write("Enter a customer: "); string customer = Console.ReadLine();
+        Console.Write("Enter a provider: "); string provider = Console.ReadLine();
+        Console.Write("Enter a quantity: "); int quantity = Convert.ToInt32(Console.ReadLine());
+        return Invoice.Create(customer, provider, quantity);
     }
 }
