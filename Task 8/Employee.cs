62cfa24e3ef40f82974335c7719dc6f73c8d1b28
Sorokovsky <Sorokovskys@ukr.net>
2024-09-11 18:38:31 +0300

    start task 8

--- Task 8/Employee.cs
+++ Task 8/Employee.cs
@@ -2,6 +2,14 @@
 {
     internal class Employee
     {
+        public string Name { get; private set; } = string.Empty;
+        public string Surname {  get; private set; } = string.Empty;
+        public Position position { get; set; } = null;
 
+        public Employee(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
     }
 }
---
+++ Task 8/Position.cs
@@ -0,0 +1,53 @@
+﻿namespace Task_8
+{
+    internal class Position
+    {
+        private int _salary, _age;
+
+        public string Name { get; private set; } = string.Empty;
+        public int Salary 
+        { 
+            get 
+            { 
+                return _salary; 
+            } 
+            set 
+            {
+                if (value < 0)
+                {
+                    _salary = 0;
+                }
+                else
+                {
+                    _salary = value;
+                }
+            } 
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _age = 0;
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        public Position(string name, int salary, int age)
+        {
+            Age = age;
+            Name = name;
+            Salary = salary;
+        }
+    }
+}
