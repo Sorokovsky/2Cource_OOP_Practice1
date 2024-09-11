385073e29e74afc00d2b1a3bcf28593bd645962b
Sorokovsky <Sorokovskys@ukr.net>
2024-09-11 20:45:17 +0300

    task 10

---
+++ Task 10/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class Program
+{
+    public static void Main()
+    {
+        
+    }
+
+    public static int ChooseOperation()
+    {
+        Console.WriteLine("Choose operation.");
+        Console.WriteLine("0-Exit.");
+        Console.WriteLine("1-Create triangle.");
+        int operation = Convert.ToInt32(Console.ReadLine());
+        return operation;
+    }
+}
---
+++ Task 10/Triangle.cs
@@ -0,0 +1,9 @@
+﻿namespace Task_10
+{
+    public class Triangle
+    {
+        public Point First { get; init; }
+        public Point Second { get; init; }
+        public Point Third { get; init; }
+    }
+}
